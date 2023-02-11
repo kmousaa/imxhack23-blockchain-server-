a733b60c86be1534e3a28171e02d169f48499caa
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2023-02-11 17:16:55 +0000

    Finish endpoints

--- express-server/app.ts
+++ express-server/app.ts
@@ -12,25 +12,49 @@ app.get('/', (req , res ) => {
   res.send('HELLO WORLD');
 });
 
+// TO : DO return the public key of the owner of the seed
+app.get('/Owner/:seed', (req, res) => {
+    const seed = req.params.seed
+    if (!seed){
+        return res.status(403).send( {'success': false} )
+    }
+    else if (typeof seed !== 'string'){
+        return res.status(403).send( {'success': false} )
+    }
+    else{
+        return res.status(201).send( {'success': true} )
+    }
+})
+
+
+// TO : DO return the list of all seeds ownded by owner
+app.get('/Seeds/:key', (req, res) => {
+    const key = req.params.key
+    if (!key){
+        return res.status(403).send( {'success': false} )
+    }
+    else if (typeof key !== 'string'){
+        return res.status(403).send( {'success': false} )
+    }
+    else{
+        return  res.status(201).send( {'success': true} )
+    }
+})
 
 app.post('/mintSeed', (req, res ) => {
     
-    console.log(req.body);
+    const { tokenID , bluePrint , publicKey  } = req.body;
 
-    const { tokenID , bluePrint, publicKey } = req.body;
-
-    if (!tokenID ||  !bluePrint || !bluePrint){
-        res.status(403).send( {'success': false} )
+    if (!tokenID ||  !bluePrint || !publicKey){
+        return res.status(403).send( {'success': false} )
     }
-    else if (tokenID !== 'string' || bluePrint !== 'string' || bluePrint !== 'string'){
-        res.status(403).send( {'success': false} )
+    else if (typeof tokenID !== 'string' || typeof bluePrint !== 'string' || typeof publicKey  !== 'string'){
+        return res.status(403).send( {'success': false} )
     }
     else {
         console.log(tokenID, bluePrint, publicKey);
-        res.status(201).send( {'success': true} )
+        return res.status(201).send( {'success': true} )
     }
-
-
 });
 
 
