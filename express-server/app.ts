1ffcc31a23b45dd3c331216329f288569ffb4349
KARIM MOUSA <karim.mousa@kcl.ac.uk>
2023-02-11 14:36:34 +0000

    Add mintSeed API

--- express-server/app.ts
+++ express-server/app.ts
@@ -4,8 +4,35 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// detect json payload 
+app.use(express.json())
+app.use(express.urlencoded( {extended :  false} ));
+
 app.get('/', (req , res ) => {
   res.send('HELLO WORLD');
 });
+
+
+app.post('/mintSeed', (req, res ) => {
+    
+    console.log(req.body);
+
+    const { tokenID , bluePrint, publicKey } = req.body;
+
+    if (!tokenID ||  !bluePrint || !bluePrint){
+        res.status(403).send( {'success': false} )
+    }
+    else if (tokenID !== 'string' || bluePrint !== 'string' || bluePrint !== 'string'){
+        res.status(403).send( {'success': false} )
+    }
+    else {
+        console.log(tokenID, bluePrint, publicKey);
+        res.status(201).send( {'success': true} )
+    }
+
+
+});
+
+
 
 app.listen(PORT, () => console.log(`started and listening on port ${PORT}.`));
